a27f7db4f9d99da0dfc08eead754d4493f8d1784
sec77 <m.kimpel@secure77.de>
2022-12-02 18:32:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# powershell-http-server
+ simple powershell http server, dont use in production!
+
+
+ start with
+
+```powershell
+.\webserver.ps1
+```
+
+- server listen on all interfaces so you need admin permissions to run the script
+- listen on port 8088
+- directory listing and navigation enabled
+- directory traversal possible
+- download and view in browser (for txt files) possible
+
+
---
+++ webserver.ps1
@@ -0,0 +1,138 @@
+$httpsrvlsnr = New-Object System.Net.HttpListener;
+$httpsrvlsnr.Prefixes.Add("http://+:8088/");
+$httpsrvlsnr.Start();
+$webroot = New-PSDrive -Name webroot -PSProvider FileSystem -Root $PWD.Path
+[byte[]]$buffer = $null
+
+function List-Files($dir) {
+    
+    $upDir = $dir.substring(0,$dir.LastIndexOf('\'))
+
+    if (!$upDir.Contains('\')) {
+        $upDir = 'C:\'
+    } 
+
+    $outPut  = "<html><a href='/stop'>Stop Server</a>`t<a href='/'>Home</a>`t<a href='/?folder=C:\'>Root (C:\)</a>`t<a href='/?folder=$($upDir)'>UP</a>"  
+    $outPut += "<pre>Current Directory: " + $dir + "<br><br>"
+    try {
+
+        $folderContent = Get-ChildItem -Path $dir -Force -ErrorAction Stop
+   
+        foreach ($item in $folderContent)
+        {
+            if (Test-Path -Path $item -PathType Container)
+            {
+                $outPut +=  "<span style=""display: inline-block; width: 50px;"">"+$item.Mode+"</span><span style=""display: inline-block; width: 150px;"">"+$item.LastWriteTime+"</span><span style=""display: inline-block; width: 120px;"">"+$item.Length+"</span><span><a href='/?folder=$($item)'>"+$item.Name+"</a></span><br>"
+            }
+            else {
+    
+                $outPut +=  "<span style=""display: inline-block; width: 50px;"">"+$item.Mode+"</span><span style=""display: inline-block; width: 150px;"">"+$item.LastWriteTime+"</span><span style=""display: inline-block; width: 120px;"">"+$item.Length+"</span><span><a href='?file=$($item)'>"+$item.Name+"</a></span> - <span><a href='?dl=$($item)'>(Download)</a></span><br>"
+            }
+        }
+        
+    }
+    catch {
+        $outPut += $Error[0]
+    }
+
+    $outPut += "</pre></html>"
+    return $outPut
+}
+
+function downloadFile($newPath) {
+    try {
+        # ... download file
+        $BUFFER = [System.IO.File]::ReadAllBytes($newPath)
+        $ctx.Response.ContentLength64 = $BUFFER.Length
+        $ctx.Response.SendChunked = $FALSE
+        $ctx.Response.ContentType = "application/octet-stream"
+        $filename = Split-Path -Leaf $newPath
+        $ctx.Response.AddHeader("Content-Disposition", "attachment; filename=$filename")
+        $ctx.Response.AddHeader("Last-Modified", [IO.File]::GetLastWriteTime($newPath).ToString('r'))
+        $ctx.Response.AddHeader("Server", "Powershell Webserver/1.1 on ")
+        $ctx.Response.OutputStream.Write($BUFFER, 0, $BUFFER.Length)
+    }
+    catch
+    {
+        $outPut += $Error[0]
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes("<html><pre>$($outPut)</pre></html>");
+        $ctx.Response.ContentLength64 = $buffer.Length;
+        $ctx.Response.OutputStream.WriteAsync($buffer, 0, $buffer.Length)
+    }
+}
+
+while ($httpsrvlsnr.IsListening) {
+    try {
+        $ctx = $httpsrvlsnr.GetContext();
+        
+        if ($ctx.Request.RawUrl -eq "/") {
+
+            $outPut = List-Files($PWD.Path)
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes("<html><pre>$($outPut)</pre></html>");
+            $ctx.Response.ContentLength64 = $buffer.Length;
+            $ctx.Response.OutputStream.WriteAsync($buffer, 0, $buffer.Length)
+
+        }
+        elseif ($ctx.Request.RawUrl -eq "/stop"){
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes("<html><pre>Server Stopped</pre></html>");
+            $ctx.Response.ContentLength64 = $buffer.Length;
+            $ctx.Response.OutputStream.WriteAsync($buffer, 0, $buffer.Length)
+            $httpsrvlsnr.Stop();
+            Remove-PSDrive -Name webroot -PSProvider FileSystem;
+        }
+        elseif ($ctx.Request.RawUrl.StartsWith("/?folder=")) {
+
+            $newPath = $ctx.Request.RawUrl.Substring(9)
+            $newPath = [System.Web.HttpUtility]::UrlDecode($newPath)
+            Write-Host "Requested folder: " $newPath 
+            if (Test-Path -Path  $newPath -PathType Container)
+            {
+                $outPut = List-Files($newPath)
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes("<html><pre>$($outPut)</pre></html>");
+                $ctx.Response.ContentLength64 = $buffer.Length;
+                $ctx.Response.OutputStream.WriteAsync($buffer, 0, $buffer.Length)
+            }
+        } # open in browser
+        elseif ($ctx.Request.RawUrl.StartsWith("/?file="))
+        {    
+            $newPath = $ctx.Request.RawUrl.Substring(7)
+            $newPath = [System.Web.HttpUtility]::UrlDecode($newPath)
+            Write-Host "Requested file: " $newPath
+            try {
+                if ([System.IO.File]::Exists(($newPath))) {
+                    $buffer = [System.Text.Encoding]::UTF8.GetBytes((Get-Content -Path $newPath));
+                    $ctx.Response.ContentLength64 = $buffer.Length;
+                    $ctx.Response.OutputStream.WriteAsync($buffer, 0, $buffer.Length)
+                }            
+            }
+            catch {
+                $outPut += $Error[0]
+                $buffer = [System.Text.Encoding]::UTF8.GetBytes("<html><pre>$($outPut)</pre></html>");
+                $ctx.Response.ContentLength64 = $buffer.Length;
+                $ctx.Response.OutputStream.WriteAsync($buffer, 0, $buffer.Length)
+            }
+       
+        } # browser download
+        elseif ($ctx.Request.RawUrl.StartsWith("/?dl="))
+        {    
+            $newPath = $ctx.Request.RawUrl.Substring(5)
+            $newPath = [System.Web.HttpUtility]::UrlDecode($newPath)
+            Write-Host "download file: " $newPath
+            downloadFile($newPath)
+         
+        } # direct downloads
+        elseif ($ctx.Request.RawUrl -match "\/[A-Za-z0-9-\s.)(\[\]]") {
+            
+            $newPath = $ctx.Request.RawUrl
+            $newPath = [System.Web.HttpUtility]::UrlDecode($newPath)
+            $newPath = Join-Path -Path $PWD.Path -ChildPath $ctx.Request.RawUrl.Trim("/\")
+            Write-Host "Direct dowload file: " $newPath
+            downloadFile($newPath)
+        }
+
+    }
+    catch [System.Net.HttpListenerException] {
+        Write-Host ($_);
+    }
+}
+
